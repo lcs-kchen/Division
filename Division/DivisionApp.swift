b0e53783cf85d0b2131bf9b39363f63f28a19ee3
Kevin Chen <kchen@lcs.on.ca>
2025-02-22 10:01:38 -0500

    Initial Commit

---
+++ Division/DivisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivisionApp.swift
+//  Division
+//
+//  Created by Haowen Chen on 2025-02-22.
+//
+
+import SwiftUI
+
+@main
+struct DivisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
