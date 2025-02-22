b3c37881197b01164364be531d80b47b1b7cb448
Kevin Chen <kchen@lcs.on.ca>
2025-02-22 10:21:09 -0500

    Added model and modelview

--- Division/ModelView/ModelView.swift
+++ Division/ModelView/ModelView.swift
@@ -7,3 +7,40 @@
 
 import Foundation
 
+class DivisionViewModel {
+    
+    //Mark: Stored Properties
+    var providedDividend: String
+    var providedDivsor: String
+    var recoverySuggestion: String
+    
+    //Mark: Computed Properites
+    var division: Division? {
+        guard let dividend = Int(providedDividend) else {
+            recoverySuggestion = "Please enter an integer for the dividend."
+            
+            return nil
+        }
+        
+        guard let divisor = Int(providedDivsor) else {
+            recoverySuggestion = "Please enter an integer for the divisor."
+            return nil }
+        
+        recoverySuggestion = ""
+        return Division(dividend: dividend, divisor: divisor)
+        
+    }
+    
+    
+    //Mark: Initializer
+    init(
+        providedDividend: String,
+        providedDivsor: String,
+        recoverySuggestion: String
+    ) {
+        self.providedDividend = providedDividend
+        self.providedDivsor = providedDivsor
+        self.recoverySuggestion = recoverySuggestion
+    }
+}
+
